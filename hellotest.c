4ebd7cfa5dd9cfcdc2e84682939ae039d536a8a7
LuTheCat <cconny2002@gmail.com>
2021-05-19 16:19:28 +0900

    thisistest2

--- hellotest.c
+++ hellotest.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
 
 int main() {
-	printf("Hello World!\nMade by ce19a002");
+	int a = 1;	
 
+	printf("Hello World!\nMade by ce19a002\n");
+	
+	if(a == 1) a++;
+	printf("%d\n", a);	
 	return 0;
 }
